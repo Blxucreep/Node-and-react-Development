9c87e60b30593750ab0dee9899929b37888b7076
Blxucreep <LoevanLqc@outlook.com>
2023-11-13 18:39:01 +0100

    Beginning of TD 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit, OnDestroy{
+  title = 'td_angular';
+  values: string ='';
+  constructor() {
+    console.log('AppComponent.constructor()')
+  }
+  ngOnInit(): void {
+    console.log('AppComponent.ngOnInit()')
+  }
+  ngOnDestroy(): void {
+    console.log('AppComponent.ngOnDestroy()')
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { FormsModule } from '@angular/forms';
+import { ChipsModule } from 'primeng/chips';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+
+    FormsModule,
+ ChipsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
