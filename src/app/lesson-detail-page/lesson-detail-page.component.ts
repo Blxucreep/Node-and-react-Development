6926c68f85fdc9e2083800c0c6651592624cc037
Blxucreep <LoevanLqc@outlook.com>
2023-11-15 17:23:12 +0100

    configuring ActivatedRoute and useHash: true

--- src/app/lesson-detail-page/lesson-detail-page.component.ts
+++ src/app/lesson-detail-page/lesson-detail-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-lesson-detail-page',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./lesson-detail-page.component.css']
 })
 export class LessonDetailPageComponent {
+  readonly initialId: number;
+  id: number | undefined;
 
+  constructor(activatedRoute: ActivatedRoute, private router: Router) {
+    this.initialId = +activatedRoute.snapshot.params['id'];
+    console.log('initialId:', this.initialId);
+    activatedRoute.params.subscribe(currParams => {
+      this.id = +currParams['id'];
+      console.log('id:', this.id);
+    });
+  }
+
+  onClickGoNextPage(){
+    const nextId = (this.id)? this.id + 1 : 1;
+    this.router.navigate(['/lesson', nextId]);
+   }   
 }
